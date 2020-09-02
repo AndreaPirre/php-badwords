5ea6fc7e5fdfbeed5fccb80085d77a8655c0af32
Andrea <andreapirrello1990@gmail.com>
2020-09-02 10:19:09 +0200

    aggiunto length e replace

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
 
-echo $str = "Lorem ipsum ciao dolor sit amet, consectetur ciao adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+$badword = $_GET['badword'];
+$parolabuona = '***';
+
+$str = "Lorem ipsum ciao dolor sit amet, consectetur ciao adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis ciao  nostrud exercitation ullamco laboris nisi ciao ut aliquip ex ea commodo ciao consequat. Duis aute irure dolor
 in reprehenderit ciao in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 Excepteur sint occaecat cupidatat ciao non proident, sunt in culpa qui officia ciao deserunt mollit anim id est laborum.";
 
-$parola = $_GET['badword'];
+$length = strlen($str);
 
-echo $strRes = "Lorem ipsum $parola dolor sit amet, consectetur $parola adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-Ut enim ad minim veniam, quis $parola  nostrud exercitation ullamco laboris nisi $parola ut aliquip ex ea commodo $parola consequat. Duis aute irure dolor
-in reprehenderit $parola in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-Excepteur sint occaecat cupidatat $parola non proident, sunt in culpa qui officia $parola deserunt mollit anim id est laborum.";
+$goodStr = str_replace($badword, $parolabuona, $str);
+
+echo $length;
+echo "<br>";
+echo $goodStr;
+
 ?>
